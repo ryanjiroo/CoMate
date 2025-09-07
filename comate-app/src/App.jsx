c25e4bfca40ac434495e8cb4529297b43d8ba4ff
Ryan Jiro <92157016+ryanjiroo@users.noreply.github.com>
2025-09-08 02:15:26 +0700

    Update App.jsx

--- comate-app/src/App.jsx
+++ comate-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Welcome from './pages/Welcome'; // Menyesuaikan path ke folder 'pages'
 import FiturUtama from './pages/FiturUtama';
+import Footer from './pages/Footer';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
       <FiturUtama/>
+      <Footer/>
     </div>
   );
 }
